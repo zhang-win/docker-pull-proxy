6fdaf8dcf16326b6a36e90df3ee36deb3bdd4166
zhang-win <37443093+zhang-win@users.noreply.github.com>
2024-10-05 04:10:26 +0800

    Update pull_updates.sh

--- pull_updates.sh
+++ pull_updates.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 BASE_URL=registry.cn-hangzhou.aliyuncs.com/outer_fast/
 CHECK_FILE=${1:-trigger.txt}
-echo git diff HEAD~:${CHECK_FILE} HEAD:${CHECK_FILE}
-git diff HEAD~:${CHECK_FILE} HEAD:${CHECK_FILE}
-lines=$(git diff HEAD^:${CHECK_FILE} HEAD:${CHECK_FILE}|grep '^+'|grep -v '^++'|awk '{print $1}')
+git log --oneline
+last_commit=$(git log --oneline|head -n 2|tail -n 1|awk '{print $1}')
+echo $last_commit
+
+lines=$(git diff ${last_commit}:${CHECK_FILE} HEAD:${CHECK_FILE}|grep '^+'|grep -v '^++'|awk '{print $1}')
 for line in $lines
 do
         line=${line:1}
